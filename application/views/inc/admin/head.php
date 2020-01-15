4e8792e2a45797211e6f4aab968737f9cdcf363b
herneyruiz36@gmail.com <herneyruiz36@gmail.com>
2020-01-15 10:05:34 -0500

    link de iconos de ion

--- application/views/inc/admin/head.php
+++ application/views/inc/admin/head.php
@@ -4,13 +4,16 @@
 
 <title>Ventas</title>
 
-<link rel="shortcut icon" href="<?php echo base_url()?>/public/src/img/sale.ico" type="image/x-icon">
+<link rel="shortcut icon" href="<?php echo base_url() ?>/public/src/img/sale.ico" type="image/x-icon">
 
 <!-- Font Awesome Icons -->
 <link rel="stylesheet" href="<?php echo base_url() ?>/public/assets/css/all.min.css">
 <link rel="stylesheet" href="<?php echo base_url() ?>/public/assets/css/jquery-ui.css">
 
-<link rel="stylesheet" href="<?php echo base_url();?>public/assets/js/dataTables-export/css/buttons.dataTables.min.css">
+<!-- Ionicons -->
+<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+<link rel="stylesheet" href="<?php echo base_url(); ?>public/assets/js/dataTables-export/css/buttons.dataTables.min.css">
 <!-- Theme style -->
 <link rel="stylesheet" href="<?php echo base_url() ?>/public/assets/css/adminlte.min.css">
 
